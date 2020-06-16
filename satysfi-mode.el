1bc8a266b2396e59145d13bfc4f046501b26b7fb
conao3 <conao3@gmail.com>
2020-06-17 02:28:56 +0900

    use % as comment-start character

--- satysfi-mode.el
+++ satysfi-mode.el
@@ -149,7 +149,8 @@
 (define-derived-mode satysfi-mode prog-mode "Satysfi"
   "Major mode for editing satysfi files."
   (set-syntax-table satysfi-mode-syntax-table)
-  (setq-local font-lock-defaults '(satysfi-mode-font-lock-keywords)))
+  (setq-local font-lock-defaults '(satysfi-mode-font-lock-keywords))
+  (setq-local comment-start "%"))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.satyh?\\'" . satysfi-mode))
