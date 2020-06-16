ce6f7bf17ecd4466fc01038f0a3f9fe56577ca2f
conao3 <conao3@gmail.com>
2020-06-17 01:08:32 +0900

    add face

--- satysfi-mode.el
+++ satysfi-mode.el
@@ -33,6 +33,63 @@
   :group 'convenience
   :link '(url-link :tag "Github" "https://github.com/conao3/satysfi-mode.el"))
 
+(defface satysfi-comment-face
+  '((t :inherit font-lock-comment-face))
+  "Face name to use for comments."
+ :group 'satysfi)
+
+(defface satysfi-comment-delimiter-face
+  '((t :inherit font-lock-comment-delimiter-face))
+  "Face name to use for comment delimiters."
+ :group 'satysfi)
+
+(defface satysfi-string-face
+  '((t :inherit font-lock-string-face))
+  "Face name to use for strings."
+ :group 'satysfi)
+
+(defface satysfi-doc-face
+  '((t :inherit font-lock-doc-face))
+  "Face name to use for documentation."
+ :group 'satysfi)
+
+(defface satysfi-keyword-face
+  '((t :inherit font-lock-keyword-face))
+  "Face name to use for keywords."
+ :group 'satysfi)
+
+(defface satysfi-builtin-face
+  '((t :inherit font-lock-builtin-face))
+  "Face name to use for builtins."
+ :group 'satysfi)
+
+(defface satysfi-function-name-face
+  '((t :inherit font-lock-function-name-face))
+  "Face name to use for function names."
+ :group 'satysfi)
+
+(defface satysfi-variable-name-face
+  '((t :inherit font-lock-variable-name-face))
+  "Face name to use for variable names."
+ :group 'satysfi)
+
+(defface satysfi-type-face
+  '((t :inherit font-lock-type-face))
+  "Face name to use for type and class names."
+ :group 'satysfi)
+
+(defface satysfi-constant-face
+  '((t :inherit font-lock-constant-face))
+  "Face name to use for constant and label names."
+ :group 'satysfi)
+
+(defface satysfi-warning-face
+  '((t :inherit font-lock-warning-face))
+  "Face name to use for things that should stand out."
+ :group 'satysfi)
+
+
+
 (define-derived-mode satysfi-mode prog-mode "Satysfi"
   "Major mode for editing satysfi files.")
 
